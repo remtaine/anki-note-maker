c4be0608c11c6bdbc26356edca5084827d7e6068
Benjo Kho <nb2kho1@gmail.com>
2015-04-04 17:13:26 +0800

    made program accept arguments in terminal

--- anki-note-maker.py
+++ anki-note-maker.py
@@ -1,6 +1,7 @@
 #!usr/bin/env python
 import time
 import os
+import sys
 
 notes_basic = []
 notes_reversible = []
@@ -9,19 +10,30 @@ notes_residue = []
 
 current_q_and_a = []
 
+args = str(sys.argv)
+
 today = time.strftime("%Y-%m-%d")
-file_name = raw_input("what should I open? ")
-file_to_open = open(file_name, 'r')
-strings = file_to_open.readlines()
+#only opens one file at a time for now
+#TODO support multiple file conversion
+if args.length == 2:
+	file_name = args[1];	
+else:
+	file_name = raw_input("what should I open? ")
+	file_to_open = open(file_name, 'r')
 
-modified_file_name = file_name.split("_")
-modified_file_name.pop(0)
-modified_file_name = "_".join(modified_file_name)
+text_lines = file_to_open.readlines()
 
-csv_basic_name = today + "_" + modified_file_name + "_notes_basic" + ".csv"
-csv_reversible_name = today + "_" + modified_file_name + "_notes_reversible" + ".csv"
-csv_cloze_name = today + "_" + modified_file_name + "_notes_cloze" + ".csv"
-csv_residue_name = today + "_" + modified_file_name + "_notes_residue" + ".csv"
+file_name_mod = file_name.split("_")
+file_name_mod.pop(0)
+file_name_mod = "_".join(file_name_mod)
+if file_name_mod.index(".") != -1:
+	#remove the extension
+	file_name_mod = file_name_mod[:file_name_mod.index(".")]
+
+csv_basic_name = today + "_" + file_name_mod + "_notes_basic" + ".csv"
+csv_reversible_name = today + "_" + file_name_mod + "_notes_reversible" + ".csv"
+csv_cloze_name = today + "_" + file_name_mod + "_notes_cloze" + ".csv"
+csv_residue_name = today + "_" + file_name_mod + "_notes_residue" + ".csv"
 
 if os.path.isfile(csv_basic_name):
 	os.remove(csv_basic_name)
@@ -37,31 +49,33 @@ csv_reversible = open(csv_reversible_name, 'w')
 csv_cloze = open(csv_cloze_name, 'w')
 csv_residue = open(csv_residue_name, 'w')
 
-csv_basic.write("tags:" + strings[0])
-csv_reversible.write("tags:" + strings[0])
-csv_cloze.write("tags:" + strings[0])
-csv_residue.write("tags:" + strings[0])
+csv_basic.write("tags:" + text_lines[0])
+csv_reversible.write("tags:" + text_lines[0])
+csv_cloze.write("tags:" + text_lines[0])
+csv_residue.write("tags:" + text_lines[0])
 
-for line in strings[1:]:
+for line in text_lines[1:]:
 	current_q_and_a = line.split('?')
 	if len(current_q_and_a) != 2: 
-		print current_q_and_a
-		print "GG"
+		print "ERROR: no '?' to signal question and answer. Aborting conversion."
+		print "Error-containing question: " + str(current_q_and_a)
 		os.remove(csv_basic_name)
 		os.remove(csv_reversible_name)
 		os.remove(csv_cloze_name)
 		os.remove(csv_residue_name)
 		break
 
-	if line[0] == ">":
+	if line[0] == "!":
 	#reversible
+	#shift + 1
 		current_q_and_a[0] = current_q_and_a[0][1:]
 		current_q_and_a[0] = current_q_and_a[0].replace(",","")
 		current_q_and_a[1] = current_q_and_a[1].replace(",","")
 		csv_reversible.write(current_q_and_a[0] + "?," + current_q_and_a[1])
 
-	elif line[0] == "<":
+	elif line[0] == "@":
 	#cloze 
+	#shift + 2
 		current_q_and_a[0] = current_q_and_a[0][1:]
 
 		#cloze deletion needs special shiz
@@ -76,34 +90,14 @@ for line in strings[1:]:
 		lineup = lineup + "\n"
 		csv_cloze.write(lineup)
 
-	#elif line[0] == "/":
-	#events
-		#basic_questions = []
-		#current_q_and_a[0] = current_q_and_a[0][1:]
-
-		#cloze deletion needs special shiz
-		#basics = current_q_and_a[1].split(',')
-		#basics[-1] = basics[-1].rstrip()
-		#for i in xrange (0,len(basics)):
-			##rip the (X) for the event #X
-			#num_start = basics[i].find("(")
-			#num_end = basics[i].find(")")
-			#event_number = basics[num_start:num_end+1]
-
-			#if i == 0:
-			##very first
-				#basic_question = "what is event " + str(event_number) + " of " + str(current_q_and_a[0]) + " that happens before " + str(basics[1]) + "?"
-			#elif i == len(basics) - 1:
-			##very last
-				#basic_question = "what is event " + str(event_number) + " of " + str(current_q_and_a[0]) + " that happens after " + str(basics[i - 1]) + "?"
-			#else:
-				#basic_question = "what is event " + str(event_number) + " of " + str(current_q_and_a[0]) + " that happens before " + str(basics[i + 1]) + " and after " + str(basics[i-1]) + "?"
-			#basic_question += str(basics)
-			#basic_question += str("\n")
-			#csv_basic.write(basic_question)
-
-	elif line[0] == "*" or line[0] == "/":
+	elif line[0] == "#":
+	#step-by-step event
+	#shift + 3
+	#TODO: actually implement
+		pass
+	elif line[0] == "$":
 	#residue
+	# shift + 4
 		current_q_and_a[0] = current_q_and_a[0].replace(",","")
 		current_q_and_a[1] = current_q_and_a[1].replace(",","")
 		csv_residue.write(current_q_and_a[0] + "?," + current_q_and_a[1])
@@ -121,8 +115,3 @@ csv_cloze.close()
 csv_residue.close()
 
 print "done"
-#first get file DONE
-#for each line in file DONE
-#separate according to question type (none, >, <) DONE
-#separate each Q&A using the ? as separator DONE
-#make CSV with those (3 separate for each question type) DONE
